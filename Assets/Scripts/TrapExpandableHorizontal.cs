0ebbb7172ed2447dae0a28285526165be71382e9
EldaZAY <elda.aiyuz@gmail.com>
2024-09-24 15:17:04 -0700

    modified map

--- Assets/Scripts/TrapExpandableHorizontal.cs
+++ Assets/Scripts/TrapExpandableHorizontal.cs
@@ -8,7 +8,7 @@ public class TrapExpandableHorizontal : Trap
     public override void ActivateSkill()
     {
         Debug.Log($"Trap {name} activates its skill: expand horizontally");
-        ExpandHorizontally();
+        ExpandHorizontally(1.5f);
     }
 
     public override void DeactivateSkill()
