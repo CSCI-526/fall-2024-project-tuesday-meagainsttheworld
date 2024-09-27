0eb2b3246084623872a6643e57866af9bc722ee1
EldaZAY <elda.aiyuz@gmail.com>
2024-09-27 16:26:15 -0700

    undo bug

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -10,8 +10,8 @@ public class PlayerHealth : MonoBehaviour
     public GameObject corpsePrefab;
     public Transform respawnPoint;
     private Rigidbody2D playerRb;
-    public GameManager gameManager; 
-    // public int life = 3;
+    public GameManager gameManager;
+    public int life = 3;
     public TextMeshProUGUI lifeText;
 
     // Start is called before the first frame update
@@ -23,32 +23,33 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if(other.gameObject.CompareTag("Traps")){
-            Die();            
+        if (other.gameObject.CompareTag("Traps"))
+        {
+            Die();
         }
     }
 
     private void Die()
     {
-    if (gameManager.isWorldWinner || GameManager.playerLives <= 0)
-    {
-        return;
-    }
-    // get a dead body
-    Quaternion corpseRotation = Quaternion.Euler(0, 0, 90);
-    Instantiate(corpsePrefab, transform.position + new Vector3(1f,0,0), corpseRotation);
-    Debug.Log("Corpse Rotation: " + corpsePrefab.transform.rotation.eulerAngles);
+        if (gameManager.isWorldWinner || GameManager.playerLives <= 0)
+        {
+            return;
+        }
+        // get a dead body
+        Quaternion corpseRotation = Quaternion.Euler(0, 0, 90);
+        Instantiate(corpsePrefab, transform.position + new Vector3(1f, 0, 0), corpseRotation);
+        Debug.Log("Corpse Rotation: " + corpsePrefab.transform.rotation.eulerAngles);
 
-    // back to spawn point
-    transform.position = respawnPoint.position;
+        // back to spawn point
+        transform.position = respawnPoint.position;
 
-    playerRb.velocity = Vector2.zero;
-    
-    gameManager.OnPlayerDeath();  // This reduces life in GameManager
+        playerRb.velocity = Vector2.zero;
+
+        gameManager.OnPlayerDeath();  // This reduces life in GameManager
     }
 }
