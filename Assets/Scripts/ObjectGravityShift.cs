a24d4b0af6d6728bfd5075fbbef460755cfa2177
EldaZAY <elda.aiyuz@gmail.com>
2024-10-06 19:31:06 -0700

    fix typo in object gravity shift

---
+++ Assets/Scripts/ObjectGravityShift.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGravityShift : MonoBehaviour
+{
+    // Flip only the current object's gravity 
+    private Rigidbody2D rb;
+    private bool isGravityFlipped = false;
+    public bool flipGravityAtStart = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        if (flipGravityAtStart)
+        {
+            rb.gravityScale = -1;
+            isGravityFlipped = true;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            // Flip the gravity scale only for this object
+            rb.gravityScale = isGravityFlipped ? 1 : -1;
+            isGravityFlipped = !isGravityFlipped;
+        }
+    }
+}
