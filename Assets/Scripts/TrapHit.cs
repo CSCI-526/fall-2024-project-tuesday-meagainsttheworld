82db2890102d46df8c9392f7c4ca5b8577f1593c
ArbitaryMouse <ziangq@usc.edu>
2024-10-18 22:28:54 -0700

    Thank you Kay

--- Assets/Scripts/TrapHit.cs
+++ Assets/Scripts/TrapHit.cs
@@ -7,6 +7,11 @@ public class TrapHit : MonoBehaviour
     {
         if (other.CompareTag("Trap"))
         {
+            //New Code by Ziang Qin: only for alpha Analytics
+            WinConditionManager.entryCount++;
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            Vector2 playerPosition = transform.position;
+            Debug.Log("Death Scene: " + currentSceneName + ", Death Player Position: " + playerPosition + ", Deaths Count: " + WinConditionManager.entryCount);
             // Change the scene to the Game Over screen
             SceneSwitcher.lastLevel = SceneManager.GetActiveScene().name;
             SceneManager.LoadScene("GameOver");
