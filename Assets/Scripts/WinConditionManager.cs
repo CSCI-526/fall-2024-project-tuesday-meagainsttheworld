b963168328fc1e558694cff700b1d86f73ca7394
kushvash <kushvash.usc@gmail.com>
2024-10-21 01:03:22 -0700

    Added player death analytics using google form

--- Assets/Scripts/WinConditionManager.cs
+++ Assets/Scripts/WinConditionManager.cs
@@ -5,7 +5,7 @@ public class WinConditionManager : MonoBehaviour
 {
     private int playersInZone = 0; // Counter for players in the win zone
     //New Code from Ziang Qin: only for alpha Analytics
-    public static int entryCount;
+    // public static int entryCount;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -23,8 +23,9 @@ public class WinConditionManager : MonoBehaviour
             if (playersInZone == 2)
             {
                 //New Code from Ziang Qin: only for alpha Analytic"s
-                Debug.Log("Total Death Count for this run of " + SceneManager.GetActiveScene().name + ": " + entryCount);
-                entryCount = 0;
+                // Debug.Log("Total Death Count for this run of " + SceneManager.GetActiveScene().name + ": " + entryCount);
+                // entryCount = 0;
+
                 SceneSwitcher.lastLevel = SceneManager.GetActiveScene().name;
                 SceneManager.LoadScene("YouWin");
             }
