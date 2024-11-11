e7dcd42a45dbd7b1819acc8db07d09db61726be4
Hardware-Man <kmishra686@gmail.com>
2024-11-11 00:56:09 -0800

    Reorganized new levels + repositioning platforms / power ups on new lvl 1 + added vertical platform oscillation

--- Assets/Scripts/HorizontalOscillator.cs
+++ Assets/Scripts/HorizontalOscillator.cs
@@ -2,10 +2,16 @@ using UnityEngine;
 
 public class HorizontalOscillator : MonoBehaviour
 {
+    public enum OscillationType
+    {
+        Horizontal,
+        Vertical
+    }
     // Public variables to control the speed and range of oscillation
     public float oscillationSpeed = 2f;  // Speed of oscillation
     public float oscillationRange = 3f;  // Horizontal distance from the start position
     [SerializeField] private bool isSinusoidal = false;
+    [SerializeField] private OscillationType oscillationType = OscillationType.Horizontal;
 
     private Rigidbody2D rb;
     private float currTime = 0;
@@ -18,7 +24,7 @@ public class HorizontalOscillator : MonoBehaviour
         // Store the initial position of the object
         startPosition = transform.position;
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = new(oscillationSpeed, 0);
+        rb.velocity = oscillationType == OscillationType.Horizontal ? new(oscillationSpeed, 0) : new(0, oscillationSpeed);
     }
 
     void FixedUpdate()
@@ -27,13 +33,21 @@ public class HorizontalOscillator : MonoBehaviour
         {
             // Calculate the new velocity using a cosine wave (using derivative based on position)
             float targetVel = oscillationSpeed * oscillationRange * Mathf.Cos(oscillationSpeed * currTime);
-            rb.velocity = new(targetVel, 0);
+            rb.velocity = oscillationType == OscillationType.Horizontal ? new(targetVel, 0) : new(0, targetVel);
             currTime += Time.fixedDeltaTime;
         }
         else
         {
-            if (transform.position.x <= startPosition.x - oscillationRange) rb.velocity = new(oscillationSpeed, 0);
-            if (transform.position.x > startPosition.x + oscillationRange) rb.velocity = new(-oscillationSpeed, 0);
+            if (oscillationType == OscillationType.Horizontal)
+            {
+                if (transform.position.x <= startPosition.x - oscillationRange) rb.velocity = new(oscillationSpeed, 0);
+                if (transform.position.x > startPosition.x + oscillationRange) rb.velocity = new(-oscillationSpeed, 0);
+            }
+            else
+            {
+                if (transform.position.y <= startPosition.y - oscillationRange) rb.velocity = new(0, oscillationSpeed);
+                if (transform.position.y > startPosition.y + oscillationRange) rb.velocity = new(0, -oscillationSpeed);
+            }
         }
     }
 }
